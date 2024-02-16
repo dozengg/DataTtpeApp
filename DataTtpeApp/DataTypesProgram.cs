3c5db5b2326163ca402f4f1a818e94c6fbc7c611
cabangon.297337 <cabangon.297337@LAB-B-PC17>
2024-02-16 09:17:54 +0800

    Add project files.

---
+++ DataTtpeApp/DataTypesProgram.cs
@@ -0,0 +1,21 @@
+﻿namespace DataTtpeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the pieces of apple: ");
+            int apple = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter total price of " +apple+ " apple(s): ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            int converprice = (int) price;
+
+            Console.WriteLine("The total price of " + apple + " apple(s)" + price);
+            Console.WriteLine("The value of original price is " + price);
+
+            Console.WriteLine("The value fo converted price is " + converprice);
+        }
+    }
+}   
